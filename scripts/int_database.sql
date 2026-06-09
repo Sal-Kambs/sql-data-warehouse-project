1debca37f1868f2bb04a7b845c985686837342c8
Sal-Kambs <salomikamble@hotmail.com>
2026-06-09 02:18:14 +0100

    Update int_database.sql

--- scripts/int_database.sql
+++ scripts/int_database.sql
@@ -1,1 +1,21 @@
+Script Purpose: This script creates a new database named 'DataWarehouse' after checking if it already exists.
+  If the database exists, it is dropped and recreated. The script then sets up 3 schemas within the database; bronze, silver, and gold
 
+USE master;
+GO
+  IF EXISTS (Select 1 From sys.databases WHERE NAME = 'DataWarehouse')
+BEGIN
+  Alter Database DataWarehouse SET Single_User With Rollback Immediate;
+Drop Database DataWarehouse;
+END;
+GO
+-- Create the 'DataWarehouse' database
+CREATE DATABASE DataWarehouse;
+USE DataWarehouse;
+
+Create schema bronze;
+GO
+Create schema silver;
+GO
+Create schema gold;
+GO
